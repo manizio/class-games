996b50b227431828db477397342f1400cf390cb8
manizio <manoel.oliveira.066@ufrn.edu.br>
2023-12-14 11:27:28 -0300

    adds simple tilemap

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-
 var SPEED = 200.0
 var interact = false
 @onready var john = $"../Ponte"
 
 var view_rect = get_viewport_rect()
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# player movement limitations
+var min_x = 0
+var max_x = 0
+var min_y = 0
+var max_y = 0
 
 func _process(delta):
 	if interact and Input.is_action_just_pressed("action"):
@@ -19,7 +23,8 @@ func _process(delta):
 		$Sprite2D.flip_h = false
 	if velocity.x < 0:
 		$Sprite2D.flip_h = true
-
+		
+	
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
@@ -42,10 +47,27 @@ func _physics_process(delta):
 		velocity.y = 0
 	
 	velocity.normalized()
-	
 	move_and_slide()
+
+	#position.x = clamp(position.x, min_x, max_x)
+	#position.y = clamp(position.y, min_y, max_y)
+
+	#$Camera2D.limit_left = min_x
+	#$Camera2D.limit_right = max_x
+	#$Camera2D.limit_top = min_y
+	#$Camera2D.limit_bottom = max_y
+	#
 	
+func limit_movement(pos_x, pos_y, end_x, end_y):
+	min_x = pos_x
+	max_x = end_x
+	
+	min_y = pos_y
+	max_y = end_y
+
 func _on_area_2d_body_entered(body):
 	interact = true
+
 func _on_area_2d_body_exited(body):
 	interact = false
+	
